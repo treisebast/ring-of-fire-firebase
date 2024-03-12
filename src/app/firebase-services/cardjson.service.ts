987b6cfdb7a6e458b09d6047a7a3313a58c0be9a
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-03-12 19:49:26 +0100

    add subGame

--- src/app/firebase-services/cardjson.service.ts
+++ src/app/firebase-services/cardjson.service.ts
@@ -1,22 +1,27 @@
 import { Injectable, inject } from "@angular/core";
-import { Firestore, doc, onSnapshot } from "@angular/fire/firestore";
+import { DocumentData, Firestore, collection, onSnapshot } from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: "root",
 })
 export class CardjsonService {
 
-  unsubgame;
-
   firestore: Firestore = inject(Firestore);
 
   constructor() {
-    this.unsubgame = onSnapshot(doc(this.firestore, "games"), (doc) => {
-      if (doc.data()) {
-        console.log(doc.data());
-      }
-    })
     
   }
-
+    
+  subGame() {
+    return onSnapshot(this.getGamesRef(), (list) => {
+      list.forEach(element => {
+        console.log(element.data());
+      });
+    });
+   }
+ 
+   getGamesRef() {
+    return collection(this.firestore, 'games');
+ }
+  
 }
